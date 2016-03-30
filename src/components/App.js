05305e234e356d8e9d3b95b35e786568302635ed
everdimension <everdimension@gmail.com>
2016-03-31 01:11:36 +0300

    add website logo; restructure layout

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { PropTypes } from 'react';
 import AnimatedBg from './AnimatedBg';
+import Logo from './Logo';
 import './App.css';
 
 class App extends React.Component {
@@ -11,21 +12,24 @@ class App extends React.Component {
 
 	render() {
 		return (
-			<div className="App content-wrapper row">
-				<AnimatedBg />
-				<header className="App__header">
-					{/* <div className="row">
-						<div className="small-4 columns">left column</div>
-						<div className="small-4 columns">
-							<span className="PageHeader__logo">
-								App
-							</span>
-						</div>
-					</div> */}
-				</header>
+			<div className="App">
+				<Logo />
+				<div className="content-wrapper row">
+					<AnimatedBg />
+					<header className="App__header">
+						{/* <div className="row">
+							<div className="small-4 columns">left column</div>
+							<div className="small-4 columns">
+								<span className="PageHeader__logo">
+									App
+								</span>
+							</div>
+						</div> */}
+					</header>
 
-				<div className="App__body">
-					{this.props.children}
+					<div className="App__body">
+						{this.props.children}
+					</div>
 				</div>
 			</div>
 		);
---
+++ src/components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React, { PropTypes } from 'react';
+import './Logo.css';
+
+class Logo extends React.Component {
+    render() {
+        return (
+            <div className="Logo">
+                <img src={require('./logo.png')}
+                    className="Logo__img"
+                    alt="check.ru logo"
+                />
+            </div>
+        );
+    }
+}
+
+export default Logo;
